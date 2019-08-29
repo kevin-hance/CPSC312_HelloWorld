0b0d3b36f6ba891084f751e8af04ed0173600beb
papahance98 <khance@zagmail.gonzaga.edu>
2019-08-29 11:28:15 -0700

    added another println statement

--- src/DemoTwo.java
+++ src/DemoTwo.java
@@ -1,5 +1,6 @@
 public class DemoTwo {
     public static void main(String[] args) {
         System.out.println("My second hello world!");
+        System.out.println("New line of text");
     }
 }
